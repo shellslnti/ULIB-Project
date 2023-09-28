b5202a8e0fe68acd874125b134ce2c6160abf6c1
shellslnti <S22110236@student.unklab.ac.id>
2023-09-28 11:14:00 +0800

    second commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,16 +30,17 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
             loginform = new Panel();
+            checkpassword = new CheckBox();
+            button2 = new Button();
+            button1 = new Button();
+            passwordbox = new TextBox();
+            passwordtxt = new Label();
+            logintxt = new Label();
+            emailbox = new TextBox();
+            emailtxt = new Label();
             pictureBox1 = new PictureBox();
             welcometxt = new Label();
-            emailtxt = new Label();
-            emailbox = new TextBox();
-            logintxt = new Label();
-            passwordtxt = new Label();
-            passwordbox = new TextBox();
-            button1 = new Button();
-            button2 = new Button();
-            checkpassword = new CheckBox();
+            label1 = new Label();
             loginform.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -48,6 +49,7 @@
             // 
             loginform.BackColor = SystemColors.ButtonHighlight;
             loginform.BorderStyle = BorderStyle.Fixed3D;
+            loginform.Controls.Add(label1);
             loginform.Controls.Add(checkpassword);
             loginform.Controls.Add(button2);
             loginform.Controls.Add(button1);
@@ -62,6 +64,92 @@
             loginform.Size = new Size(446, 401);
             loginform.TabIndex = 0;
             loginform.Paint += loginform_Paint;
+            // 
+            // checkpassword
+            // 
+            checkpassword.AutoSize = true;
+            checkpassword.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            checkpassword.ForeColor = SystemColors.ActiveCaptionText;
+            checkpassword.Location = new Point(155, 231);
+            checkpassword.Name = "checkpassword";
+            checkpassword.Size = new Size(97, 19);
+            checkpassword.TabIndex = 7;
+            checkpassword.Text = "See password";
+            checkpassword.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(322, 289);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 33);
+            button2.TabIndex = 6;
+            button2.Text = "submit";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(229, 289);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 33);
+            button1.TabIndex = 5;
+            button1.Text = "cancel";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // passwordbox
+            // 
+            passwordbox.Location = new Point(155, 202);
+            passwordbox.Name = "passwordbox";
+            passwordbox.Size = new Size(242, 24);
+            passwordbox.TabIndex = 4;
+            // 
+            // passwordtxt
+            // 
+            passwordtxt.AutoSize = true;
+            passwordtxt.BackColor = Color.Transparent;
+            passwordtxt.Font = new Font("Bahnschrift Light", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            passwordtxt.ForeColor = SystemColors.ActiveCaptionText;
+            passwordtxt.Location = new Point(37, 207);
+            passwordtxt.Name = "passwordtxt";
+            passwordtxt.Size = new Size(81, 19);
+            passwordtxt.TabIndex = 3;
+            passwordtxt.Text = "Password";
+            // 
+            // logintxt
+            // 
+            logintxt.BackColor = Color.Yellow;
+            logintxt.Font = new Font("Tahoma", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            logintxt.ForeColor = Color.Purple;
+            logintxt.Location = new Point(-2, 0);
+            logintxt.Name = "logintxt";
+            logintxt.Size = new Size(453, 76);
+            logintxt.TabIndex = 2;
+            logintxt.Text = "U-LIB LOGIN";
+            logintxt.TextAlign = ContentAlignment.MiddleCenter;
+            logintxt.Click += label1_Click;
+            // 
+            // emailbox
+            // 
+            emailbox.Location = new Point(155, 139);
+            emailbox.Name = "emailbox";
+            emailbox.Size = new Size(242, 24);
+            emailbox.TabIndex = 1;
+            // 
+            // emailtxt
+            // 
+            emailtxt.AutoSize = true;
+            emailtxt.BackColor = Color.Transparent;
+            emailtxt.Font = new Font("Bahnschrift Light", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            emailtxt.ForeColor = SystemColors.ActiveCaptionText;
+            emailtxt.Location = new Point(37, 138);
+            emailtxt.Name = "emailtxt";
+            emailtxt.Size = new Size(51, 19);
+            emailtxt.TabIndex = 0;
+            emailtxt.Text = "Email";
+            emailtxt.Click += emailtxt_Click;
             // 
             // pictureBox1
             // 
@@ -88,91 +176,18 @@
             welcometxt.TabIndex = 2;
             welcometxt.Text = "WELCOME TO U-LIB";
             // 
-            // emailtxt
+            // label1
             // 
-            emailtxt.AutoSize = true;
-            emailtxt.BackColor = Color.Transparent;
-            emailtxt.Font = new Font("Bahnschrift Light", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            emailtxt.ForeColor = SystemColors.ActiveCaptionText;
-            emailtxt.Location = new Point(37, 138);
-            emailtxt.Name = "emailtxt";
-            emailtxt.Size = new Size(51, 19);
-            emailtxt.TabIndex = 0;
-            emailtxt.Text = "Email";
-            emailtxt.Click += emailtxt_Click;
-            // 
-            // emailbox
-            // 
-            emailbox.Location = new Point(155, 139);
-            emailbox.Name = "emailbox";
-            emailbox.Size = new Size(242, 24);
-            emailbox.TabIndex = 1;
-            // 
-            // logintxt
-            // 
-            logintxt.BackColor = Color.Yellow;
-            logintxt.Font = new Font("Tahoma", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            logintxt.ForeColor = Color.Purple;
-            logintxt.Location = new Point(-2, 0);
-            logintxt.Name = "logintxt";
-            logintxt.Size = new Size(453, 64);
-            logintxt.TabIndex = 2;
-            logintxt.Text = "U-LIB LOGIN";
-            logintxt.TextAlign = ContentAlignment.MiddleCenter;
-            logintxt.Click += label1_Click;
-            // 
-            // passwordtxt
-            // 
-            passwordtxt.AutoSize = true;
-            passwordtxt.BackColor = Color.Transparent;
-            passwordtxt.Font = new Font("Bahnschrift Light", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            passwordtxt.ForeColor = SystemColors.ActiveCaptionText;
-            passwordtxt.Location = new Point(37, 207);
-            passwordtxt.Name = "passwordtxt";
-            passwordtxt.Size = new Size(81, 19);
-            passwordtxt.TabIndex = 3;
-            passwordtxt.Text = "Password";
-            // 
-            // passwordbox
-            // 
-            passwordbox.Location = new Point(155, 202);
-            passwordbox.Name = "passwordbox";
-            passwordbox.Size = new Size(242, 24);
-            passwordbox.TabIndex = 4;
-            // 
-            // button1
-            // 
-            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.ForeColor = SystemColors.ActiveCaptionText;
-            button1.Location = new Point(229, 289);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 33);
-            button1.TabIndex = 5;
-            button1.Text = "cancel";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.ForeColor = SystemColors.ActiveCaptionText;
-            button2.Location = new Point(322, 289);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 33);
-            button2.TabIndex = 6;
-            button2.Text = "submit";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // checkpassword
-            // 
-            checkpassword.AutoSize = true;
-            checkpassword.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            checkpassword.ForeColor = SystemColors.ActiveCaptionText;
-            checkpassword.Location = new Point(155, 231);
-            checkpassword.Name = "checkpassword";
-            checkpassword.Size = new Size(97, 19);
-            checkpassword.TabIndex = 7;
-            checkpassword.Text = "See password";
-            checkpassword.UseVisualStyleBackColor = true;
+            label1.AutoSize = true;
+            label1.BackColor = Color.Yellow;
+            label1.Font = new Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(165, 53);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 14);
+            label1.TabIndex = 3;
+            label1.Text = "Login as Student";
             // 
             // login
             // 
@@ -211,5 +226,6 @@
         private Button button2;
         private Button button1;
         private CheckBox checkpassword;
+        private Label label1;
     }
 }
